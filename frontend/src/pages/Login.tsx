6f6e31aad3c6dcddd5fc7056295006feb899a353
BenchPress200 <jms0538@naver.com>
2025-07-12 16:29:27 +0900

    feat: add join page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,39 @@
+import { 
+    Container, 
+    LoginBox, 
+    Title, 
+    InputForm, 
+    Input, 
+    ButtonBox, 
+    LoginButton,
+    KakaoButton,
+    JoinButton,
+} from "../styles/pages/Login.styles";
+
+
+
 const Login = () => {
     return (
-        <></>
+        <Container>
+            <LoginBox>
+                <Title>Login</Title>
+
+                <InputForm>
+                    <Input placeholder="name">
+                    </Input>
+                    <Input placeholder="password">
+                    </Input>
+                </InputForm>
+
+                <ButtonBox>
+                    <LoginButton>login</LoginButton>
+                    <KakaoButton>kakao Login</KakaoButton>
+                    <JoinButton>join</JoinButton>
+                </ButtonBox>
+
+            </LoginBox>
+
+        </Container>
     )
 }
 
