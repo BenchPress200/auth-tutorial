f13a7be02de88d0852a9189aca8786f62ffee0b6
BenchPress200 <jms0538@naver.com>
2025-07-13 13:38:17 +0900

    feat: connect user,auth api

--- frontend/src/pages/Join.tsx
+++ frontend/src/pages/Join.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
     Container,
     JoinBox,
@@ -8,12 +10,66 @@ import {
     JoinButton,
     LoginButton,
 } from "../styles/pages/Join.styles";
+import { ResponseBody } from '../types/api';
 import { useNavigation } from '../hooks/useNavigation';
 
 
 
 const Join = () => {
     const { handleNavigateToLogin } = useNavigation();
+
+    const [nameInput, setNameInput] = useState<string>('');
+    const [passwordInput, setPasswordInput] = useState<string>('');
+
+
+
+    const handleNameInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNameInput(e.target.value);
+    };
+
+    const handlePasswordInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPasswordInput(e.target.value);
+    };
+
+    const handleJoinRequest = () => {
+        const body = {
+            name: nameInput,
+            password: passwordInput,
+        }
+        
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+
+        fetch('http://localhost:8080/api/v1/users', requestOptions)
+            .then((response: Response) => {
+                if(response.ok) {
+                    alert('[회원가입 완료]');
+                    handleNavigateToLogin();
+                    return;
+                }
+                
+                response.json().then((responseBody: ResponseBody) => {
+                    alert(`
+                    [회원가입 실패]
+                    status: ${responseBody.status}
+                    message: ${responseBody.message}
+                    timestamp: ${responseBody.timestamp}`)
+                })
+                
+            })
+            .catch((error: Error) => {
+                console.error(error.message);
+            })
+            .finally();
+    }
+
+
 
     return (
         <Container>
@@ -23,15 +79,19 @@ const Join = () => {
                     <Input 
                         placeholder="name"
                         type="text"
+                        value={nameInput}
+                        onChange={handleNameInputChange}
                     ></Input>
                     <Input 
                         placeholder="password"
                         type="password"
+                        value={passwordInput}
+                        onChange={handlePasswordInputChange}
                     ></Input>
                 </InputForm>
 
                 <ButtonBox>
-                    <JoinButton>join</JoinButton>
+                    <JoinButton onClick={handleJoinRequest}>join</JoinButton>
                     <LoginButton onClick={handleNavigateToLogin}>login</LoginButton>
                 </ButtonBox>
             </JoinBox>
